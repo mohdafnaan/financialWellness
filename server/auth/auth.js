5934224b6201b57ce7b25eecaa5e66405c9d013b
mohdafnaan <mohdafnaan833@gmail.com>
2026-02-06 19:07:44 +0530

    dist aded

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -2,18 +2,18 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config()
 
-function middleware(req,res,next){
+function middleware(req, res, next) {
     try {
         let token = req.headers.authorization?.split(' ')[1];
-        if(!token){
-            res.status(400).json({msg: "invalid token"})
+        if (!token) {
+            return res.status(401).json({ msg: "Token required" });
         }
-        console.log(process.env.JWT_SECKEY)
-        const decode = jwt.verify(token,process.env.JWT_SECKEY);
+        const decode = jwt.verify(token, process.env.JWT_SECKEY);
         req.user = decode;
-        next()
+        next();
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        return res.status(401).json({ msg: "Invalid or expired token" });
     }
 }
 export default middleware
